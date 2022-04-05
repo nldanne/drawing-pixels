62d8e90388d1edcb8fd360541d787bc028e5b7df
nldanne <nldanne97@gmail.com>
2022-04-05 13:13:45 +0200

    created Pixel component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Pixel from './Pixel';
+import '../styles/row.scss';
 
-function Row() {
+function Row({ width, selectedColor }) {
+  let pixels = [];
+
+  for (let i = 0; i < width; i++) {
+    pixels.push(<Pixel key={i} selectedColor={selectedColor} />);
+  }
+
   return (
-    <div className='row'></div>
+    <div className='row'>{pixels}</div>
   )
 }
 
