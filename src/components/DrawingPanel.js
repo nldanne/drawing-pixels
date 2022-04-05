84bc657342006d90835331f1fe61c381649f353f
nldanne <nldanne97@gmail.com>
2022-04-05 14:20:13 +0200

    added donwload buttons

--- src/components/DrawingPanel.js
+++ src/components/DrawingPanel.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import '../styles/drawingPanel.scss';
+import React, { useRef } from 'react';
 import Row from './Row';
+import { exportComponentAsPNG, exportComponentAsJPEG } from 'react-component-export-image';
+
+import '../styles/drawingPanel.scss';
 
 function DrawingPanel({ width, height, selectedColor }) {
+  const componentRef = useRef();
+  
+  
   let rows = [];
 
   for (let i = 0; i < height; i++) {
@@ -11,9 +16,19 @@ function DrawingPanel({ width, height, selectedColor }) {
 
   return (
     <div id="drawing-panel">
-      <div id="pixels">
+      <div id="pixels" ref={componentRef}>
         {rows}
       </div>
+      <button 
+        className='button-outline'
+        onClick={() => exportComponentAsPNG(componentRef)}
+      > Export as PNG
+      </button>
+      <button 
+        className='button-outline'
+        onClick={() => exportComponentAsJPEG(componentRef)}
+      > Export as JPG
+      </button>
     </div>
   )
 }
