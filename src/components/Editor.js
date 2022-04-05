cafc8a3a41cf04bb300da4f0bf0cf64a2347a6b7
nldanne <nldanne97@gmail.com>
2022-04-05 12:03:48 +0200

    set up editor inputs

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import '../styles/editor.scss';
 
 function Editor() {
   return (
-    <div id="editor"></div>
+    <div id="editor">
+      <h1>Pixel Art Editor</h1>
+      <h2>Enter Your Canvas Dimensions</h2>
+      <div id="options">
+        <div className='option'>
+          <input 
+            type='number' 
+            className='panelInput' 
+            defaultValue="16" 
+            //onChange={}
+          />
+          <span>Width</span>
+        </div>
+        <div className='option'>
+          <input 
+            type='number' 
+            className='panelInput' 
+            defaultValue="16" 
+            //onChange={}
+          />
+          <span>Height</span>
+        </div>
+      </div>
+
+      <button 
+        className='button'
+        // onClick={}
+      > Start drawing
+      </button>
+    </div>
   )
 }
 
