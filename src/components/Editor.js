ebb8e190d37f25e690ac2c0e34e90d176edd9fe6
nldanne <nldanne97@gmail.com>
2022-04-05 13:11:44 +0200

    initialized states, imported DrawingPanel component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CirclePicker } from 'react-color';
+import DrawingPanel from './DrawingPanel';
+
 import '../styles/editor.scss';
 
 function Editor() {
+  const [canvasWidth, setCanvasWidth] = useState(16);
+  const [canvasHeight, setCanvasHeight] = useState(16);
+  const [hideOptions, setHideOptions] = useState(false);
+  const [hideDrawingCanvas, setHideDrawingCanvas] = useState(true);
+  const [buttonText, setButtonText] = useState('Start drawing');
+  const [selectedColor, setSelectedColor] = useState('#f44336');
+
+  const widthHandler = (e) => {
+    setCanvasWidth(e.target.value);
+  };
+
+  const heightHandler = (e) => {
+    setCanvasHeight(e.target.value);
+  };
+
+  const initializeCanvas = () => {
+    setHideOptions(!hideOptions);
+    setHideDrawingCanvas(!hideDrawingCanvas);
+
+    buttonText ==='Start drawing' 
+      ? setButtonText('reset') 
+      : setButtonText('Start drawing');
+  };
+
+  const changeColorHandler = (color) => {
+    setSelectedColor(color.hex);
+    console.log(selectedColor);
+  };
+
   return (
     <div id="editor">
       <h1>Pixel Art Editor</h1>
-      <h2>Enter Your Canvas Dimensions</h2>
+      {hideDrawingCanvas && <h2>Enter Your Canvas Dimensions</h2>}
+      {hideDrawingCanvas && (
       <div id="options">
         <div className='option'>
           <input 
             type='number' 
             className='panelInput' 
-            defaultValue="16" 
-            //onChange={}
+            defaultValue={canvasWidth} 
+            onChange={widthHandler}
           />
           <span>Width</span>
         </div>
@@ -20,19 +53,30 @@ function Editor() {
           <input 
             type='number' 
             className='panelInput' 
-            defaultValue="16" 
-            //onChange={}
+            defaultValue={canvasHeight} 
+            onChange={heightHandler}
           />
           <span>Height</span>
         </div>
-      </div>
+      </div> )}
 
       <button 
         className='button'
-        // onClick={}
-      > Start drawing
+        onClick={initializeCanvas}
+      > {buttonText}
       </button>
-    </div>
+
+      {hideDrawingCanvas && (<CirclePicker 
+        color={selectedColor} 
+        onChangeComplete={changeColorHandler}
+      />  )}
+
+      <DrawingPanel 
+        width={canvasWidth}
+        height={canvasHeight}
+        selectedColor={selectedColor}
+      />
+    </div> 
   )
 }
 
