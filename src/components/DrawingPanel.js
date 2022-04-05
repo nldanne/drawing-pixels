24af595f83b03429ac3b778324ac90c8458802c8
nldanne <nldanne97@gmail.com>
2022-04-05 13:12:38 +0200

    added props to component

--- src/components/DrawingPanel.js
+++ src/components/DrawingPanel.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import '../styles/drawingPanel.scss';
+import Row from './Row';
 
-function DrawingPanel() {
+function DrawingPanel({ width, height, selectedColor }) {
+  let rows = [];
+
+  for (let i = 0; i < height; i++) {
+    rows.push(<Row key={i} width={width}  selectedColor={selectedColor} />)
+  }
+
   return (
-    <div id="drawing-panel"></div>
+    <div id="drawing-panel">
+      <div id="pixels">
+        {rows}
+      </div>
+    </div>
   )
 }
 
